496f68556396df736edc8d52e9eec9803912e908
Ketz99 <julian.gomez.perez45@gmail.com>
2026-07-17 14:01:08 -0500

    Activación Proyecto

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,7 @@
-const supabaseUrl = 'https://xqkrmcakdpgfmqwdiszs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa3JtY2FrZHBnZm1xd2Rpc3pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3MzcwMzYsImV4cCI6MjA3MzMxMzAzNn0.M3lXDQuk_VxERH39_KKPeq_JMYUpeoa1CJal6cmfE3s';
+const supabaseUrl = 'https://vmibgwpkzymqnlldhepx.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaWJnd3BrenltcW5sbGRoZXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjk2MTcsImV4cCI6MjA5OTgwNTYxN30.pQ5n0EOwk_-QGgyulYrVb7XoE27tNVS9tzUSfVGIG-4';
 
 // En lugar de declarar "const supabase", tomamos la librería del CDN 
 // y sobrescribimos la variable global window.supabase con tu cliente ya conectado.
 window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+console.log("¡Cliente conectado exitosamente al nuevo entorno de Supabase!");
